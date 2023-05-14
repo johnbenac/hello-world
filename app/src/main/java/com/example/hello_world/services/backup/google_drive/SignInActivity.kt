1000c975af645d0f4f446c3e6fec1f9d0c6e1a77
johnbenac <john@benac.com>
2023-05-13 20:04:56 -0400

    authenticating with new activity

---
+++ app/src/main/java/com/example/hello_world/services/backup/google_drive/SignInActivity.kt
@@ -0,0 +1,27 @@
+package com.example.hello_world.services.backup.google_drive
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hello_world.R
+
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var googleDriveBackupHelper: GoogleDriveBackupHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        googleDriveBackupHelper = GoogleDriveBackupHelper(this)
+        googleDriveBackupHelper.authenticate()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GoogleDriveBackupHelper.RC_SIGN_IN) {
+            googleDriveBackupHelper.handleSignInResult(resultCode, data)
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/hello_world/ui/google_drive_backup/viewmodel/GoogleDriveBackupViewModel.kt
+++ app/src/main/java/com/example/hello_world/ui/google_drive_backup/viewmodel/GoogleDriveBackupViewModel.kt
@@ -1,16 +1,18 @@
 package com.example.hello_world.ui.google_drive_backup.viewmodel
 
 import android.app.Activity
+import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import com.example.hello_world.services.backup.google_drive.GoogleDriveBackupHelper
+import com.example.hello_world.services.backup.google_drive.SignInActivity
 
 class GoogleDriveBackupViewModel(private val activity: Activity) : ViewModel() {
     private val googleDriveBackupHelper = GoogleDriveBackupHelper(activity)
-
     fun authenticate() {
         Log.d("GoogleDriveBackupVM", "authenticate() called")
-        googleDriveBackupHelper.authenticate()
+        val signInIntent = Intent(activity, SignInActivity::class.java)
+        activity.startActivity(signInIntent)
     }
 
     fun backup() {
